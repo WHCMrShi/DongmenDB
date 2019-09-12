38f505bfcf3d203fe657a6763f201bdb0f86d854
WHCMrShi <whcmrshi@live.cn>
2019-09-13 01:10:10 +0800

    实现 delete 功能

--- src_experiment/exp_01_stmt_parser/exp_01_05_delete.cpp
+++ src_experiment/exp_01_stmt_parser/exp_01_05_delete.cpp
@@ -1,5 +1,6 @@
 //
 // Created by Sam on 2018/2/13.
+// Completed by Shils Shek on 2019/9/10.
 //
 
 #include <dongmensql/sqlstatement.h>
@@ -17,5 +18,62 @@
  */
 
 sql_stmt_delete *DeleteParser::parse_sql_stmt_delete(){
+    char *tableName = NULL;
+    SRA_t *where;
+    sql_stmt_delete *sqlStmtDelete = (sql_stmt_delete *)calloc(sizeof(sql_stmt_delete),1);
 
+    Token *token = this->parseNextToken();
+    if(!this->matchToken(TOKEN_RESERVED_WORD, "delete")){
+        strcpy(this->parserMessage, "grammatical error: missing delete.");
+        return NULL;
+    }
+
+    token = this->parseNextToken();
+    if (token->type == TOKEN_WORD) {
+        tableName = new_id_name();
+        strcpy(tableName, token->text);
+    } else {
+        strcpy(this->parserMessage, "invalid sql: missing table name.");
+        return NULL;
+    }
+    TableReference_t *ref = TableReference_make(tableName, NULL);
+
+    token = this->parseNextToken();
+    if(token == NULL || token->type == TOKEN_SEMICOLON){
+        /*无where，update语句结束，对各字段赋值，并返回*/
+        where = SRATable(ref);
+        sqlStmtDelete->tableName = tableName;
+        sqlStmtDelete->where = where;
+        return sqlStmtDelete;
+    }
+
+
+    //token = this->parseNextToken();
+    token = this->parseEatAndNextToken();
+    if(!this->matchToken(TOKEN_RESERVED_WORD, "where")){
+        strcpy(this->parserMessage, "grammatical error: missing where.");
+        return NULL;
+    }
+
+    /*解析where子句中的条件表达式*/
+    token = this->parseNextToken();
+    Expression *whereExpr = this->parseExpressionRD();
+    if (this->parserStateType == PARSER_WRONG) {
+        return NULL;
+    }
+    where = SRATable(ref);
+    where = SRASelect(where, whereExpr);
+    token = this->parseNextToken();
+
+    if (token == NULL || token->type == TOKEN_SEMICOLON) {
+        sqlStmtDelete->tableName = tableName;
+        sqlStmtDelete->where = where;
+        /*
+        printf("\nToken Recognized: \ntableName: %s\n",tableName);
+        printf("where: ");
+        SRA_print(where);
+        putchar('\n');
+        */
+        return sqlStmtDelete;
+    }
 };
--- src_experiment/exp_07_physical_operate/exp_07_06_delete.cpp
+++ src_experiment/exp_07_physical_operate/exp_07_06_delete.cpp
@@ -1,11 +1,8 @@
 //
 // Created by sam on 2018/9/18.
+// Completed by Shils Shek on 2019/9/10.
 //
 #include <physicalplan/ExecutionPlan.h>
-#include <physicalplan/TableScan.h>
-#include <physicalplan/Select.h>
-#include <physicalplan/Project.h>
-#include <physicalplan/Join.h>
 
 /*执行delete语句的物理计划，返回删除的记录条数
  * 返回大于等于0的值，表示删除的记录条数；
@@ -13,9 +10,14 @@
  * */
 
 int ExecutionPlan::executeDelete(DongmenDB *db, sql_stmt_delete *sqlStmtDelete, Transaction *tx){
-        /*删除语句以select的物理操作为基础实现。
-     * 1. 使用 sql_stmt_delete 的条件参数，调用 physical_scan_select_create 创建select的物理计划并初始化;
-     * 2. 执行 select 的物理计划，完成 delete 操作
-     * */
-    return -1;
+    size_t deleted_count = 0;
+    Scan* scan = generateScan(db, sqlStmtDelete->where, tx);
+    scan->beforeFirst();
+
+    while(scan->next()) {
+        scan->deleteRecord();
+        deleted_count++;
+    }
+    scan->close();
+    return deleted_count;
 };
