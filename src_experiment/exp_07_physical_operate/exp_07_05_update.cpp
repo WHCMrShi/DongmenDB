44900f9718a24e56dadbd6761adab149757cc788
WHCMrShi <whcmrshi@live.cn>
2019-09-13 01:08:22 +0800

    实现 update 功能

--- src_experiment/exp_07_physical_operate/exp_07_05_update.cpp
+++ src_experiment/exp_07_physical_operate/exp_07_05_update.cpp
@@ -1,24 +1,37 @@
 //
 // Created by sam on 2018/9/18.
+// Completed by Shils Shek on 2019/9/10.
 //
-
 #include <physicalplan/ExecutionPlan.h>
-#include <physicalplan/TableScan.h>
-#include <physicalplan/Select.h>
-#include <physicalplan/Project.h>
-#include <physicalplan/Join.h>
-
-/*执行 update 语句的物理计划，返回修改的记录条数
- * 返回大于等于0的值，表示修改的记录条数；
- * 返回小于0的值，表示修改过程中出现错误。
- * */
-/*TODO: plan_execute_update， update语句执行*/
-
+#include <physicalplan/Scan.h>
 
 int ExecutionPlan::executeUpdate(DongmenDB *db, sql_stmt_update *sqlStmtUpdate, Transaction *tx){
-    /*删除语句以select的物理操作为基础实现。
-     * 1. 使用 sql_stmt_update 的条件参数，调用 physical_scan_select_create 创建select的物理计划并初始化;
-     * 2. 执行 select 的物理计划，完成update操作
-     * */
-    return -1;
+
+    size_t updated_count = 0;
+    Scan* scan = generateScan(db, sqlStmtUpdate->where, tx);
+    scan->beforeFirst();
+
+    while(scan->next()) {
+        for (size_t i = 0; i < sqlStmtUpdate->fieldsExpr.size(); i++) {
+            char *currentFieldName = sqlStmtUpdate->fields.at(i);
+            variant *val = (variant *) calloc(sizeof(variant), 1);
+            int field_type = scan->getField(sqlStmtUpdate->tableName, currentFieldName)->type;
+
+            scan->evaluateExpression(sqlStmtUpdate->fieldsExpr.at(i), scan, val);
+
+            if (val->type != field_type) {
+                fprintf(stderr, "field type error\n");
+                return DONGMENDB_EINVALIDSQL;
+            }
+
+            if (val->type == DATA_TYPE_INT) {
+                scan->setInt(sqlStmtUpdate->tableName, currentFieldName, val->intValue);
+            } else if (val->type == DATA_TYPE_CHAR) {
+                scan->setString(sqlStmtUpdate->tableName, currentFieldName, val->strValue);
+            }
+        }
+        updated_count++;
+    }
+    scan->close();
+    return updated_count;
 };
